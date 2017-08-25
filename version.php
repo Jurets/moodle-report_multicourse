4e3efd571185182c47d6e58b07c84fbbc5ef8115
jurets <jurets75@gmail.com>
2017-08-25 17:56:40 +0300

    https://academysmart.teamwork.com/#tasks/7913613 * commit initial files

---
+++ version.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of the Multi Course Grader report for Moodle by Barry Oosthuizen http://elearningstudio.co.uk
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details for the multi grader report
+ *
+ * @package   report_multicourse
+ * @copyright 2017 Jurets
+ * @author    Jurets
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2017081700;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires = 2017050500;        // Requires this Moodle version
+$plugin->component = 'report_multicourse'; // Full name of the plugin (used for diagnostics)
+//$plugin->maturity  = MATURITY_BETA;
+//$plugin->release   = '0.9.4';
